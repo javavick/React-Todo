f2267dc6074f2fb80bd3554dcc16020956cb578f
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-10-31 01:10:17 -0400

    Build TodoForm Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React from "react";
+import TodoForm from "./components/TodoComponents/TodoForm.js";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  /*========== STATE ==========*/
+  constructor() {
+    super();
+    this.state = {
+      task: "",
+      data: []
+    };
+  }
+
+  /*========== HANDLERS ==========*/
+  handleChange = (event) => {
+    this.setState({ task: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      data: [
+        ...this.state.data,
+        {
+          task: this.state.task,
+          id: Date.now(),
+          completed: false
+        }
+      ]
+    });
+    console.log(this.state);
+    event.target.reset();
+  };
+
+  /*========== RENDER ==========*/
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+        />
       </div>
     );
   }
